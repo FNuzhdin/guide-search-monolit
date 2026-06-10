40aacd93f23957473379f9910504251e8918dffb
Fedor_Monolit <fakemail@gmail.com>
2026-06-10 15:16:04 +0300

    checkpoint 10: next.config was changed

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://test.monolit-calculator.ru/api/:path*',
-      },
-    ];
-  },
-
 };
 
 export default nextConfig;
